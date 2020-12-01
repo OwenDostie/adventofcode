acfc82e2d7c82e47576a0a410f96d4acecd48c5d
OwenDostie <owendostie@gmail.com>
2020-12-01 13:20:09 -0500

    solve d1p2

--- day1/main.py
+++ day1/main.py
@@ -1,9 +1,28 @@
+from itertools import combinations
 f = open("input.txt","r")
+f = list(map(int, f.read().splitlines()))
+
 seeking = set()
-for n in f:
-    # idk how to read n as an int straight from f
-    n = int(n)
-    if n in seeking: 
-        print(n * (2020 - n))
+
+def findPair(sumsTo):
+    for n in f:
+        if n in seeking: return (n * (sumsTo - n))
+        seeking.add(sumsTo - n)
+
+def findTriplet():
+    for i1 in range(len(f)):
+        subTotal = 2020-f[i1]
+        seeking = set()
+        for i2 in range(i1, len(f)):
+            if f[i2] in seeking:
+                return f[i1]*f[i2]*(subTotal - f[i2])
+            seeking.add(subTotal - f[i2])
+
+print("Part one:", findPair(2020))
+print("Part two:", findTriplet())
+
+print("Using itertools combinations:")
+for combo in combinations(f,3):
+    if sum(combo) == 2020:
+        print(combo[0]*combo[1]*combo[2])
         break
-    seeking.add(2020 - n)
